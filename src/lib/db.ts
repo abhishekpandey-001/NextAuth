a11b114e6f696c863721913dd88195854ab05957
Abhishek Pandey <abhishekpandey.codes@gmail.com>
2026-06-14 23:51:47 +0530

    added connection string to connect with mongoose

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,32 @@
+import { connect } from "mongoose";
+
 const mongodbUrl = process.env.MONGO_URI;
 if (!mongodbUrl) {
   throw new Error("MongoDb URL is not found");
 }
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+const connectDb = async () => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = connect(mongodbUrl).then((c) => c.connection);
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (error) {
+    throw error;
+  }
+
+  return cached.conn;
+};
+
+export default connectDb;
